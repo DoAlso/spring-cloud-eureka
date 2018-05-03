3c39cf75639714f2c2fcde913df997881ed72db2
admin <315238976@qq.com>
2018-05-03 12:32:40 +0800

    ....................

--- eureka-netty/src/main/java/com/eureka/netty/core/FreeSwitchClient.java
+++ eureka-netty/src/main/java/com/eureka/netty/core/FreeSwitchClient.java
@@ -1,60 +1,60 @@
-//package com.eureka.netty.core;
-//
-//import com.eureka.common.utils.LogUtil;
-//import com.eureka.netty.config.FreeSwitchProperties;
-//import com.eureka.netty.listener.EslCoreEventListener;
-//import org.freeswitch.esl.client.inbound.Client;
-//import org.freeswitch.esl.client.inbound.InboundConnectionFailure;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-//import javax.annotation.PostConstruct;
-//import javax.annotation.PreDestroy;
-//
-///**
-// * @author 胡亚曦
-// * 该类通过FreeSwitch的
-// * Socket_Event事件控制
-// * FreeSwitch
-// */
-//@Component
-//public class FreeSwitchClient {
-//    private static Logger logger = LoggerFactory.getLogger(FreeSwitchClient.class);
-//    @Autowired
-//    private FreeSwitchProperties properties;
-//    @Autowired
-//    private EslCoreEventListener coreEventListener;
-//    private Client client = new Client();
-//
-//    @PostConstruct
-//    public void start(){
-//        /**
-//         * 由于freeswitch连接操作会阻塞主线程，
-//         * 所以此处开启一个线程用于连接FreeSwitch
-//         * 服务，防止主线程的阻塞
-//         */
-//        try {
-//            LogUtil.info(logger,"开始执行FreeSwitch服务端连接操作......");
-//            client.connect(properties.getHost(),properties.getPort(),properties.getPassword(),properties.getTimeoutSec());
-//            client.addEventListener(coreEventListener);
-//            client.setEventSubscriptions("plain", "all");
-//            LogUtil.info(logger,"FreeSwitch服务端连接成功......");
-//        }catch (InboundConnectionFailure e){
-//            LogUtil.error(logger,"FreeSwitch服务连接失败,错误信息是:{}",e.getMessage());
-//        }
-//    }
-//
-//    public Client getClient() {
-//        return client;
-//    }
-//
-//    @PreDestroy
-//    public void close(){
-//        if(client != null){
-//            client.close();
-//        }
-//        LogUtil.info(logger,"FreeSwitch连接关闭成功...");
-//    }
-//}
+package com.eureka.netty.core;
+
+import com.eureka.common.utils.LogUtil;
+import com.eureka.netty.config.FreeSwitchProperties;
+import com.eureka.netty.listener.EslCoreEventListener;
+import org.freeswitch.esl.client.inbound.Client;
+import org.freeswitch.esl.client.inbound.InboundConnectionFailure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * @author 胡亚曦
+ * 该类通过FreeSwitch的
+ * Socket_Event事件控制
+ * FreeSwitch
+ */
+@Component
+public class FreeSwitchClient {
+    private static Logger logger = LoggerFactory.getLogger(FreeSwitchClient.class);
+    @Autowired
+    private FreeSwitchProperties properties;
+    @Autowired
+    private EslCoreEventListener coreEventListener;
+    private Client client = new Client();
+
+    @PostConstruct
+    public void start(){
+        /**
+         * 由于freeswitch连接操作会阻塞主线程，
+         * 所以此处开启一个线程用于连接FreeSwitch
+         * 服务，防止主线程的阻塞
+         */
+        try {
+            LogUtil.info(logger,"开始执行FreeSwitch服务端连接操作......");
+            client.connect(properties.getHost(),properties.getPort(),properties.getPassword(),properties.getTimeoutSec());
+            client.addEventListener(coreEventListener);
+            client.setEventSubscriptions("plain", "all");
+            LogUtil.info(logger,"FreeSwitch服务端连接成功......");
+        }catch (InboundConnectionFailure e){
+            LogUtil.error(logger,"FreeSwitch服务连接失败,错误信息是:{}",e.getMessage());
+        }
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    @PreDestroy
+    public void close(){
+        if(client != null){
+            client.close();
+        }
+        LogUtil.info(logger,"FreeSwitch连接关闭成功...");
+    }
+}
--- eureka-users/src/test/java/com/eureka/users/EurekaUsersApplicationTests.java
+++ eureka-users/src/test/java/com/eureka/users/EurekaUsersApplicationTests.java
@@ -1,14 +1,35 @@
 package com.eureka.users;
 
+import com.eurake.common.params.LoginParam;
+import com.eureka.common.model.BaseParam;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EurekaUsersApplicationTests {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+
     @Test
     public void contextLoads() {
     }
+
+    @Test
+    public void testLogin(){
+        BaseParam<LoginParam> baseParam = new BaseParam<>();
+        LoginParam loginParam = new LoginParam();
+        loginParam.setAccount("admin");
+        loginParam.setPassword("123456");
+        baseParam.setParams(loginParam);
+        String result = this.testRestTemplate.postForEntity("/login",baseParam,String.class).getBody();
+        System.out.println(result);
+    }
+
 }
